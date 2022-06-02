f55895cfa2be0c485d9bd4a54a448ab5d0f669c6
Jorge Alia <jorgealia94@gmail.com>
2022-06-02 19:50:42 +0200

    completed printing the components

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Display } from './display';
 
 export function Actions() {
---
+++ src/components/key.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export function Key({ item }: { item: string }) {
+    return (
+        <>
+            <button className={item === 'delete' ? 'key big' : 'key'}>
+                {item}
+            </button>
+        </>
+    );
+}
